ae0b9d259c9b0f5a22703f4667b28a33bfc4e33f
Litl Toast <joshuafhiggins@gmail.com>
2021-07-30 02:55:44 -0400

    For the rest of the tutorials after

--- tutorial15/resources/shaders/mainVertex.glsl
+++ tutorial15/resources/shaders/mainVertex.glsl
@@ -12,7 +12,7 @@ uniform mat4 view;
 uniform mat4 projection;
 
 void main() {
-	gl_Position = projection * view * model * vec4(position, 1.0);
+	gl_Position = vec4(position, 1.0) * model * view * projection;
 	passColor = color;
 	passTextureCoord = textureCoord;
 }
